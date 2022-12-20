46a51f89d841ee13d1b78fd3e992a910b7be7144
Matt Nishi-Broach <go.for.dover@gmail.com>
2022-12-19 22:30:17 -0500

    feat: move to vim-plug, add vim-coc

--- install_vim.sh
+++ install_vim.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-ln -s ~/.vim/.vimrc ~/.vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+ln -s ~/.vim/.vimrc ~/.vimrc 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim -es -u vimrc -i NONE -c "PlugInstall" -c "qa"
+vim -c 'CocInstall -sync coc-json coc-html coc-css coc-docker coc-eslint coc-go coc-jedi coc-prettier coc-sh coc-tsserver coc-yaml|q'
